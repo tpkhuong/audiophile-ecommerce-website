7b0460981de93be9c70eac5261ca2bd66647b4fe
Toan <tpkhuong@gmail.com>
2023-01-29 03:57:03 -0500

    getting next build to work.

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -10,77 +10,82 @@ import CategoryImg from "../../Components/Category/CategoryImgWrapper";
 import CategoryTextContent from "../../Components/Category/CategoryTextContent";
 import MissionStatement from "../../Components/shared/MissionStatement";
 import Footer from "../../Components/shared/Footer";
+import ErrorPage from "next/error";
 import { server } from "../../config/index";
 import axios from "axios";
 
 function CategoryPage({ children, ...props }) {
-  const firstLetter = props.categoryStr[0].toUpperCase();
-  const restOfLetters = props.categoryStr.slice(1);
-  const pageTitle = [firstLetter, ...restOfLetters].join("");
+  if (!props.data && !props.categoryStr && !props.arrayOfUrls) {
+    return <ErrorPage statusCode={404} />;
+  } else {
+    const firstLetter = props.categoryStr[0].toUpperCase();
+    const restOfLetters = props.categoryStr.slice(1);
+    const pageTitle = [firstLetter, ...restOfLetters].join("");
 
-  return (
-    <React.Fragment>
-      {/* category items container: will be container of our div. The div */}
-      {/* will have two children: one img wrapper and one text content container */}
-      {/* category items container will have display flex to controll spacing between elements */}
-      <Head>
-        <title>{pageTitle}</title>
-        <link
-          rel="shortcut icon"
-          href="/favicon-32x32.png"
-          type="image/x-icon"
-        />
-      </Head>
-      <a className="skip-link" href="#main-content">
-        Skip to Main Content
-      </a>
-      <h1 className="visually-hidden">{pageTitle}</h1>
-      <header role="banner">
-        <LogoNavContainer />
-        <BannerTitle>{props.categoryStr}</BannerTitle>
-      </header>
-      <Main>
-        <article className={CategoryPageStyles[`products-container`]}>
-          {/* use map loop through array make div with two children: one img wrapper and one text content */}
-          {props.data.map(function makeProductContent(element, index) {
-            // use destructuring
-            const { imgSrc, imgText, textContent } = element;
-            const { desktop, tablet, mobile } = imgSrc;
-            const { isNew, name, description } = textContent;
-            const findIsRight = index % 2;
-            return (
-              <div
-                key={Math.random() * index}
-                className={CategoryPageStyles[`img-textcontent-wrapper`]}
-              >
-                {/* img wrapper */}
-                {/* component takes these props: desktopSize, tabletSize, mobileSize, text */}
-                <CategoryImg
-                  desktopSize={desktop}
-                  tabletSize={tablet}
-                  mobileSize={mobile}
-                  text={imgText}
-                />
-                {/* text content */}
-                {/* component takes these props: rightSide, isNew, title,content */}
-                <CategoryTextContent
-                  rightSide={findIsRight == 0 ? true : false}
-                  isNew={isNew}
-                  title={name}
-                  content={description}
-                  productUrl={props.arrayOfUrls[index]}
-                />
-              </div>
-            );
-          })}
-        </article>
-        <CategoryCardWrapper pageStyle="category" />
-        <MissionStatement pageMargin="category" />
-      </Main>
-      <Footer />
-      <MobileNav />
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        {/* category items container: will be container of our div. The div */}
+        {/* will have two children: one img wrapper and one text content container */}
+        {/* category items container will have display flex to controll spacing between elements */}
+        <Head>
+          <title>{pageTitle}</title>
+          <link
+            rel="shortcut icon"
+            href="/favicon-32x32.png"
+            type="image/x-icon"
+          />
+        </Head>
+        <a className="skip-link" href="#main-content">
+          Skip to Main Content
+        </a>
+        <h1 className="visually-hidden">{pageTitle}</h1>
+        <header role="banner">
+          <LogoNavContainer />
+          <BannerTitle>{props.categoryStr}</BannerTitle>
+        </header>
+        <Main>
+          <article className={CategoryPageStyles[`products-container`]}>
+            {/* use map loop through array make div with two children: one img wrapper and one text content */}
+            {props.data.map(function makeProductContent(element, index) {
+              // use destructuring
+              const { imgSrc, imgText, textContent } = element;
+              const { desktop, tablet, mobile } = imgSrc;
+              const { isNew, name, description } = textContent;
+              const findIsRight = index % 2;
+              return (
+                <div
+                  key={Math.random() * index}
+                  className={CategoryPageStyles[`img-textcontent-wrapper`]}
+                >
+                  {/* img wrapper */}
+                  {/* component takes these props: desktopSize, tabletSize, mobileSize, text */}
+                  <CategoryImg
+                    desktopSize={desktop}
+                    tabletSize={tablet}
+                    mobileSize={mobile}
+                    text={imgText}
+                  />
+                  {/* text content */}
+                  {/* component takes these props: rightSide, isNew, title,content */}
+                  <CategoryTextContent
+                    rightSide={findIsRight == 0 ? true : false}
+                    isNew={isNew}
+                    title={name}
+                    content={description}
+                    productUrl={props.arrayOfUrls[index]}
+                  />
+                </div>
+              );
+            })}
+          </article>
+          <CategoryCardWrapper pageStyle="category" />
+          <MissionStatement pageMargin="category" />
+        </Main>
+        <Footer />
+        <MobileNav />
+      </React.Fragment>
+    );
+  }
 }
 
 export default CategoryPage;
@@ -105,11 +110,21 @@ export async function getStaticProps(context) {
   // make api calls here
 
   /**
+   * using fetch
+   * **/
+  const response = await fetch(`${server}/api/${context.params.category}`);
+  const data = await response.json();
+
+  /**
    * using axios
    * **/
 
-  const response = await axios(`${server}/api/${context.params.category}`);
-  const { data } = response;
+  // const response = await axios(`${server}/api/${context.params.category}`);
+  // const { data } = response;
+
+  /**
+   * without api call
+   * **/
 
   const arrayOfProductNames = data.map(function getName(obj) {
     return obj.textContent.name;
